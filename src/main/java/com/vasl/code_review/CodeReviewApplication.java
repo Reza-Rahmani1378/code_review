44a1d309ac1a87b7ad9665500faf35939fd7b0f5
Reza <reza6273757@gmail.com>
2022-10-25 18:16:43 +0330

    Refactor

--- src/main/java/com/vasl/code_review/CodeReviewApplication.java
+++ src/main/java/com/vasl/code_review/CodeReviewApplication.java
@@ -2,10 +2,14 @@ package com.vasl.code_review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @ComponentScan("com.vasl")
+@EnableMongoAuditing
+@EnableFeignClients
 public class CodeReviewApplication {
 
 	public static void main(String[] args) {
